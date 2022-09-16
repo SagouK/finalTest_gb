e3c68770b91a6a4c1b857377c5a97358eacebd31
Sagou <anastasiyazadybinina@gmailcom>
2022-09-16 17:39:52 +0300

    create array!

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив
+// из строк, длина которых меньше, либо равна 3 символа.
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+//string[] array = CreateArray(size);
+Console.Write(String.Join(",", CreateArray(size)));
+
+string[] CreateArray(int length)
+{
+    string[] arr = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine("Введите строчный элемент массива: ");
+        arr[i] = Console.ReadLine() ?? "";
+    }
+    return arr;
+}
+
+
